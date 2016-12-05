a499b010ed6b01ac9d7421b70b11dab618e1a8ca
David Trail <david@vaunt.eu>
2016-12-05 11:06:59 +0000

    Day 5.2 and all caught up!

--- 5.rb
+++ 5.rb
@@ -1,17 +1,24 @@
 require 'digest'
 
 input = 'abbhdwsy'
-# input = 'abc'
-output = String.new
+output = Array.new(8)
+output.fill(false)
+
 i = 0
-while output.length < 8 do
+while output.include? false do
   md5 = Digest::MD5.new
-  md5 << input + i.to_s
-  # puts "#{input + i.to_s} - #{md5.hexdigest}"
-  if md5.hexdigest[0..4] == '00000' then
-    output += md5.hexdigest[5]
+  md5 << (input + i.to_s)
+  hexdigest = md5.hexdigest()
+  if hexdigest[0..4] == '00000' then
+    pos = hexdigest[5]
+    if ("0".."7").include? pos then
+      if output[pos.to_i(16)] != false then
+        i += 1
+        next
+      end
+      output[pos.to_i] = md5.hexdigest[6]
+      puts "#{output.join} - #{pos} - #{hexdigest[5..6]}"
+    end
   end
   i += 1
 end
-
-puts output
