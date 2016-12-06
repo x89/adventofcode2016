3e6541e721a2f65fa7e627c3a77c4d606ad01640
David Trail <david@vaunt.eu>
2016-12-06 09:24:38 +0000

    6.1 done

--- 6.rb
+++ 6.rb
@@ -1,14 +1,27 @@
 fh = open('6input', mode='r')
-lines = Array.new
+lines = []
 fh.readlines().each do |line|
   lines.push(line.chomp)
 end
 fh.close()
 
 # Use array of hashes to store character frequency
+frequency_array = 8.times.map {{}}
+
 lines.each do |line|
-  puts line
   line.chars.each_with_index do |c,idx|
-    puts "#{idx}: #{c}"
+    hash = frequency_array[idx]
+    if hash.include? c then
+      hash[c] += 1
+    else
+      hash[c] = 1
+    end
+    # puts "Added char #{c} to hash id #{idx} to make #{hash[c]}"
   end
 end
+
+frequency_array.each do |h|
+  print h.sort_by{|k,v|v}.to_a[-1][0]
+end
+
+puts
